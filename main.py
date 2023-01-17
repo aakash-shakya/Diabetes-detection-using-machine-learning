09889dac58e835d0ebaf75e21089feefba88ed0d
aakash-shakya <official.aakas@gmail.com>
2023-01-17 17:33:15 +0545

    changes to layout

--- main.py
+++ main.py
@@ -22,21 +22,30 @@ def user_data(raw_input_data):
         return('person is not diabetic')
 
 
+
+# streamlit page config
+st.set_page_config(page_title='Diabetes Prediction Web App', page_icon=':syringe:', layout='wide', initial_sidebar_state='auto')
+
+
 # giving a title
 st.title('Diabetes Prediction Web App')
 
+# st columns of 3
+col1, col2, col3 = st.columns(3)
+
 
 # getting the input data from the user
-
-
-Pregnancies = st.text_input('Number of Pregnancies', placeholder="Enter the number of pregnancies")
-Glucose = st.text_input('Glucose Level', placeholder="Enter the Glucose Level")
-BloodPressure = st.text_input('Blood Pressure value', placeholder="Enter the Blood Pressure value")
-SkinThickness = st.text_input('Skin Thickness value', placeholder="Enter the Skin Thickness value")
-Insulin = st.text_input('Insulin Level',    placeholder="Enter the Insulin Level")
-BMI = st.text_input('BMI value', placeholder="Enter the BMI value")
-DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value', placeholder="Enter the Diabetes Pedigree Function value")
-Age = st.text_input('Age of the Person', placeholder="Enter the Age of the Person")
+with col1:
+    Pregnancies = st.text_input('Number of Pregnancies', placeholder="Enter the number of pregnancies")
+    Glucose = st.text_input('Glucose Level', placeholder="Enter the Glucose Level")
+    BloodPressure = st.text_input('Blood Pressure value', placeholder="Enter the Blood Pressure value")
+with col2:
+    SkinThickness = st.text_input('Skin Thickness value', placeholder="Enter the Skin Thickness value")
+    Insulin = st.text_input('Insulin Level',    placeholder="Enter the Insulin Level")
+    BMI = st.text_input('BMI value', placeholder="Enter the BMI value")
+with col3:
+    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value', placeholder="Enter the Diabetes Pedigree Function value")
+    Age = st.text_input('Age of the Person', placeholder="Enter the Age of the Person")
 
 
 # code for Prediction
